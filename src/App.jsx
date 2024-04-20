f6b91753779496c2ed3fecfb50caafcc2dfad552
Tosinfamzy <famurewatosin@gmail.com>
2024-04-20 12:50:05 +0100

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline'>
+				Blessed hope site under construction
+			</h1>
+		</>
+	);
+}
+
+export default App;
